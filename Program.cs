9d8a6808f156bd7070292cd9cf9f02e7c8d1ce25
kaelandersson <kaeldev@outlook.com>
2023-08-09 12:55:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml;
+
+using (XmlReader reader = XmlReader.Create(@"../../../xml/sma_gentext.xml"))
+{
+    string outputPath = @"../../../txt/";
+
+    while (reader.Read())
+    {
+        if (reader.IsStartElement())
+        {
+                if (reader.Name == "trans-unit")
+                {
+                    while (reader.MoveToNextAttribute())
+                    {
+                        if (reader.Name == "id" && reader.Value == "42007")
+                        {
+                            reader.MoveToElement();
+                            reader.ReadToDescendant("target");
+                            string target = reader.ReadElementContentAsString();
+                            Console.WriteLine("Target value: {0}", target);
+                            File.WriteAllText(outputPath + "output.txt", target);
+                        }
+                    }
+                    reader.MoveToElement();
+                }
+                reader.Read();           
+        }
+    }
+
+    Console.WriteLine("\nPress any button to exit.");
+    Console.ReadKey();
+}
